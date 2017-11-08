93680ccab37a832dd26185bac4586051a1336891
hai@leftcoastlogic.com <hai@leftcoastlogic.com>
2017-11-08 10:02:07 +0700

    - Share scheme ZFToken

---
+++ Demo/ZFTokenField/ZFTokenField.h
@@ -0,0 +1,19 @@
+//
+//  ZFTokenField.h
+//  ZFTokenField
+//
+//  Created by Nguyen Thanh Hai on 11/8/17.
+//  Copyright © 2017 Amornchai Kanokpullwad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZFTokenField.
+FOUNDATION_EXPORT double ZFTokenFieldVersionNumber;
+
+//! Project version string for ZFTokenField.
+FOUNDATION_EXPORT const unsigned char ZFTokenFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZFTokenField/PublicHeader.h>
+
+
