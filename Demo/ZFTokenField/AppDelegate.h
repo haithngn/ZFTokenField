daf04ed8892582f04138bd6a1c36617d4d48dbef
hai@leftcoastlogic.com <hai@leftcoastlogic.com>
2017-11-08 10:00:28 +0700

    - Share scheme ZFToken

---
+++ Demo/ZFTokenField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZFTokenField
+//
+//  Created by Nguyen Thanh Hai on 11/8/17.
+//  Copyright © 2017 Amornchai Kanokpullwad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
