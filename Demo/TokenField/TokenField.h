fef34d07845ab405eb95b90b8cee03c6f6d77922
hai@leftcoastlogic.com <hai@leftcoastlogic.com>
2017-11-09 09:39:58 +0700

    - Share scheme ZFToken

---
+++ Demo/TokenField/TokenField.h
@@ -0,0 +1,19 @@
+//
+//  TokenField.h
+//  TokenField
+//
+//  Created by Nguyen Thanh Hai on 11/9/17.
+//  Copyright © 2017 Amornchai Kanokpullwad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TokenField.
+FOUNDATION_EXPORT double TokenFieldVersionNumber;
+
+//! Project version string for TokenField.
+FOUNDATION_EXPORT const unsigned char TokenFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TokenField/PublicHeader.h>
+
+
